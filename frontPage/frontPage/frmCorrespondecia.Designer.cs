4e7711a8e0364c0c978664ed380474d26e55a215
Sadaw9 <pedro.saldanha117@gmail.com>
2020-09-26 15:21:45 -0300

    Email

--- frontPage/frontPage/frmCorrespondecia.Designer.cs
+++ frontPage/frontPage/frmCorrespondecia.Designer.cs
@@ -29,21 +29,194 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCorrespondecia));
+            this.lblPara = new System.Windows.Forms.Label();
+            this.lblCc = new System.Windows.Forms.Label();
+            this.lblCco = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblAssunto = new System.Windows.Forms.Label();
+            this.lblAnexos = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.btnAnexos = new System.Windows.Forms.Button();
+            this.file = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.file)).BeginInit();
             this.SuspendLayout();
             // 
-            // funcao2
+            // lblPara
+            // 
+            this.lblPara.AutoSize = true;
+            this.lblPara.Location = new System.Drawing.Point(311, 37);
+            this.lblPara.Name = "lblPara";
+            this.lblPara.Size = new System.Drawing.Size(42, 17);
+            this.lblPara.TabIndex = 0;
+            this.lblPara.Text = "Para:";
+            // 
+            // lblCc
+            // 
+            this.lblCc.AutoSize = true;
+            this.lblCc.Location = new System.Drawing.Point(325, 66);
+            this.lblCc.Name = "lblCc";
+            this.lblCc.Size = new System.Drawing.Size(28, 17);
+            this.lblCc.TabIndex = 1;
+            this.lblCc.Text = "Cc:";
+            // 
+            // lblCco
+            // 
+            this.lblCco.AutoSize = true;
+            this.lblCco.Location = new System.Drawing.Point(317, 91);
+            this.lblCco.Name = "lblCco";
+            this.lblCco.Size = new System.Drawing.Size(36, 17);
+            this.lblCco.TabIndex = 2;
+            this.lblCco.Text = "Cco:";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(374, 34);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(389, 22);
+            this.maskedTextBox1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(374, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(389, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(374, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(389, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // lblAssunto
+            // 
+            this.lblAssunto.AutoSize = true;
+            this.lblAssunto.Location = new System.Drawing.Point(8, 158);
+            this.lblAssunto.Name = "lblAssunto";
+            this.lblAssunto.Size = new System.Drawing.Size(63, 17);
+            this.lblAssunto.TabIndex = 6;
+            this.lblAssunto.Text = "Assunto:";
+            // 
+            // lblAnexos
+            // 
+            this.lblAnexos.AutoSize = true;
+            this.lblAnexos.Location = new System.Drawing.Point(13, 193);
+            this.lblAnexos.Name = "lblAnexos";
+            this.lblAnexos.Size = new System.Drawing.Size(58, 17);
+            this.lblAnexos.TabIndex = 7;
+            this.lblAnexos.Text = "Anexos:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(91, 158);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(672, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(91, 289);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(672, 178);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Location = new System.Drawing.Point(8, 292);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(81, 17);
+            this.lblMensagem.TabIndex = 11;
+            this.lblMensagem.Text = "Mensagem:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(91, 193);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(486, 68);
+            this.listBox1.TabIndex = 12;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(16, 13);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(260, 123);
+            this.btnEnviar.TabIndex = 13;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // btnAnexos
+            // 
+            this.btnAnexos.Location = new System.Drawing.Point(583, 193);
+            this.btnAnexos.Name = "btnAnexos";
+            this.btnAnexos.Size = new System.Drawing.Size(180, 68);
+            this.btnAnexos.TabIndex = 14;
+            this.btnAnexos.Text = "Anexar";
+            this.btnAnexos.UseVisualStyleBackColor = true;
+            this.btnAnexos.Click += new System.EventHandler(this.btnAnexos_Click);
+            // 
+            // file
+            // 
+            this.file.EnableRaisingEvents = true;
+            this.file.SynchronizingObject = this;
+            // 
+            // frmCorrespondecia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 484);
+            this.Controls.Add(this.btnAnexos);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.lblAnexos);
+            this.Controls.Add(this.lblAssunto);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.lblCco);
+            this.Controls.Add(this.lblCc);
+            this.Controls.Add(this.lblPara);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "funcao2";
-            this.Text = "funcao2";
+            this.Name = "frmCorrespondecia";
+            this.Text = "Correspondência";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmCorrespondecia_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.file)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblPara;
+        private System.Windows.Forms.Label lblCc;
+        private System.Windows.Forms.Label lblCco;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblAssunto;
+        private System.Windows.Forms.Label lblAnexos;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Button btnAnexos;
+        private System.IO.FileSystemWatcher file;
     }
 }
--- frontPage/frontPage/frmCorrespondecia.cs
+++ frontPage/frontPage/frmCorrespondecia.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Mail;
+using System.Web;
+using System.IO;
 
 namespace frontPage
 {
@@ -15,6 +18,68 @@ namespace frontPage
         public frmCorrespondecia()
         {
             InitializeComponent();
+        }
+        SmtpClient smtp = new SmtpClient();
+        MailMessage mail = new MailMessage();
+
+
+        private void frmCorrespondecia_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new System.Net.NetworkCredential("condoframe@gmail.com", "tcccondoframe");
+
+            mail.From = new MailAddress("condoframe@gmail.com");
+            if(!string.IsNullOrWhiteSpace(maskedTextBox1.Text))
+            {
+                mail.To.Add(maskedTextBox1.Text);
+            }
+            else
+            {
+                MessageBox.Show("Campo Obrigatório");
+                return;
+            }
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                mail.CC.Add(textBox1.Text);
+            }
+            if (!string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                mail.Bcc.Add(textBox2.Text);
+            }
+            mail.Subject = textBox3.Text;
+            mail.Body = richTextBox1.Text;
+            mail.IsBodyHtml = true;
+
+            foreach (string file in listBox1.Items)
+            {
+                mail.Attachments.Add(new System.Net.Mail.Attachment(file));
+            }
+
+            smtp.Send(mail);
+            MessageBox.Show("Email Enviado");
+        }
+
+        private void btnAnexos_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Multiselect = true;
+                if (dialog.ShowDialog() == DialogResult.OK) ;
+                {
+                    foreach (var file in dialog.FileName);
+                    {
+                        listBox1.Items.Add(file);
+                    }
+                }
+            }
         }
     }
 }
